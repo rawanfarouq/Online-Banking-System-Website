55659533c3f68a27eee31d76c6a0a9f198716e33
BasselTharwat <bissosamir@gmail.com>
2023-05-19 23:04:38 +0300

    login & signup

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ else{
     alert('Loan Successful')
 }
 }
+function loginCredentials() {
+    if (document.getElementById("Username").value == "client" && document.getElementById("Password").value == "client") {
+      window.location.assign('Accounts.html');
+    } else if (document.getElementById("Username").value == "admin" && document.getElementById("Password").value == "admin") {
+      window.location.assign('admin.html');
+    } else if (document.getElementById("Username").value == "banker" && document.getElementById("Password").value == "banker") {
+      window.location.assign('banker.html');
+    } else {
+      alert('Please enter valid credentials');
+    }
+  }
+
+function signUp(){
+    if(document.getElementById("SignUpUsername").value=="" || document.getElementById("SignUpPassword").value=="" ){
+        alert('Please enter valid credentials')
+    }
+    else{
+        location.replace('/Login.html');
+    }
+   
+}  
