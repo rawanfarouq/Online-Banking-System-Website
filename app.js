426b8fbf1179de4144fe6ba5b810c0f8f15e7c3c
shahdsharaf <shahdsharaf02@gmail.com>
2023-05-23 20:41:27 +0300

    app.js update

--- app.js
+++ app.js
@@ -2,47 +2,24 @@ function addCheckbox() {
   var checkboxList = document.getElementById("checkboxList");
   var newCheckboxInput = document.getElementById("newCheckboxInput");
 
-  // Create a new checkbox element
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.name = "checkboxItem";
-  checkbox.value = newCheckboxInput.value;
+  // Get the value of the input field
+  var inputValue = newCheckboxInput.value;
 
-  // Create a label element for the checkbox
-  var label = document.createElement("label");
-  label.appendChild(document.createTextNode(newCheckboxInput.value));
-
-  // Create a list item element
-  var listItem = document.createElement("li");
-
-  // Append the checkbox and label to the list item
-  listItem.appendChild(checkbox);
-  listItem.appendChild(label);
-
-  // Append the list item to the checkbox list
-  checkboxList.appendChild(listItem);
-
-  // Clear the input field
-  newCheckboxInput.value = "";
-}
-function removeCheckbox(checkbox) {
-  var listItem = checkbox.parentNode;
-  var checkboxList = listItem.parentNode;
-  checkboxList.removeChild(listItem);
-}
-function addCheckbox() {
-  var checkboxList = document.getElementById("checkboxList");
-  var newCheckboxInput = document.getElementById("newCheckboxInput");
+  // Check if the input value is empty
+  if (inputValue.trim() === "") {
+    alert("Cannot have an empty reminder.");
+    return;
+  }
 
   // Create a new checkbox element
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.name = "checkboxItem";
-  checkbox.value = newCheckboxInput.value;
+  checkbox.value = inputValue;
 
   // Create a label element for the checkbox
   var label = document.createElement("label");
-  label.appendChild(document.createTextNode(newCheckboxInput.value));
+  label.appendChild(document.createTextNode(inputValue));
 
   // Create a list item element
   var listItem = document.createElement("li");
@@ -54,8 +31,13 @@ function addCheckbox() {
     removeCheckbox(checkbox);
   };
 
-  // Append the checkbox, label, and remove button to the list item
+  // Create a span element for spacing
+  var spacingSpan = document.createElement("span");
+  spacingSpan.style.marginRight = "10px"; // Adjust the margin value as needed
+
+  // Append the checkbox, spacing span, label, and remove button to the list item
   listItem.appendChild(checkbox);
+  listItem.appendChild(spacingSpan);
   listItem.appendChild(label);
   listItem.appendChild(removeButton);
 
@@ -64,16 +46,37 @@ function addCheckbox() {
 
   // Clear the input field
   newCheckboxInput.value = "";
-} 
+}
+function ValidateSelection()  
+       {  
+           var check_box = document.getElementsByName("reminders");  
+           var CheckedItems = 0; 
+           for(var i = 0; i < check_box.length; i++)  
+           {  
+               if(check_box[i].checked)  
+                   CheckedItems++;  
+           }  
+       }  
 
-function removeSelectedItem() {
+
+function removeCheckbox(checkbox) {
+  var listItem = checkbox.parentNode;
+  var checkboxList = listItem.parentNode;
+  checkboxList.removeChild(listItem);
+}
+
+function removeSelectedBill() {
   var dropdown = document.getElementById("myDropdown");
   var selectedIndex = dropdown.selectedIndex;
 
-  if (selectedIndex !== -1) {
+  if (selectedIndex !== -1 && selectedIndex !== 0) {
     dropdown.remove(selectedIndex);
+    alert("Bill paid");
+  } else {
+    alert("Please select a bill to pay");
   }
 }
+
 function currentAccountDetails() {
 
     var container = document.getElementById("accountDetails");
